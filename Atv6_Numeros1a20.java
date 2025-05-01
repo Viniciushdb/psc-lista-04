2edb8b24f395c968fdc914c09a0705e057b2f4e3
Viniciushdb <Viniciusdiniz430@gmail.com>
2025-04-30 21:39:04 -0300

    Atv6_Numeros1a20.java

---
+++ Atv6_Numeros1a20.java
@@ -0,0 +1,13 @@
+public class Numeros1a20 {
+    public static void main(String[] args) {
+        System.out.println("Números um abaixo do outro:");
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("\nNúmeros um ao lado do outro:");
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + " ");
+        }
+    }
+} 
