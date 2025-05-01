a215a3b0ebc1c15d3b719fce25cdb52195b68f0f
Viniciushdb <Viniciusdiniz430@gmail.com>
2025-04-30 21:42:36 -0300

    Atv9_NumerosImpares.java

---
+++ Atv9_NumerosImpares.java
@@ -0,0 +1,11 @@
+public class NumerosImpares {
+    public static void main(String[] args) {
+        System.out.println("Números ímpares entre 1 e 50:");
+        
+        for (int i = 1; i <= 50; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+} 
